97e0e95decefcc0b36447cbc7aff0c25e179b36f
unknown <etienne.coutaud@MW7L6IIABNM2OG.dir.svc.accenture.com>
2015-02-19 14:17:28 +0100

    update jar*

--- src/dockerMain/mainDocker.java
+++ src/dockerMain/mainDocker.java
@@ -1,12 +1,9 @@
 package dockerMain;
 
-import static spark.Spark.get;
 
 public class mainDocker {
 
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            return "hello from sparkjava.com";
-        });
+      System.out.println("Hello Docker");
     }
 }
